2b45d70a90fe30d78d620c5ddcfd13cfbb97589a
AdriaMelero1 <adriamelero1@gmail.com>
2024-01-09 17:03:24 +0100

    react blog page started (display blogs)

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,7 +1,50 @@
+
+// const Inicio = () => {
+
+// 	const [nombre, setNombre] = useState("Adria");
+// 	const [edad, setEdad] = useState(20);
+
+// 	const handleClick = () => {
+
+// 		setNombre("Pedro");
+// 		setEdad(25);
+// 	}
+
+// 	const handleReset = () => {
+// 		setNombre("Adria");
+// 		setEdad(20);
+// 	}
+
+
+// 	return (
+// 		<div className="home">
+// 			<h2>Página de inicio</h2>
+// 			<p>{ nombre } tiene { edad } años</p>
+// 			<button onClick={handleClick }>Dale!</button>
+// 			<button onClick={handleReset }>Reset!</button>
+// 		</div>
+// 	);
+// }
+
+import { useState } from "react";
+
 const Inicio = () => {
+
+	const [blogs, setBlogs] = useState([
+		{ titulo: "Mi nueva web", body: "Lorem ipsum,,,", autor: "Antonio", id: 1 },
+		{ titulo: "Fiesta del Ramis", body: "Mucha paella,,,", autor: "Enrique", id: 2 },
+		{ titulo: "GP de belgica", body: "Round 15,,,", autor: "Lobato", id: 3 },
+
+	])
+
 	return (
 		<div className="home">
-			<h2>Página de inicio</h2>
+			{blogs.map((blog) =>
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.titulo}</h2>
+					<p>Escrito por {blog.autor}</p>
+				</div>
+			)}
 		</div>
 	);
 }
